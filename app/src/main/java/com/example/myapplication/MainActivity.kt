614a5bb4a71f721aacd2d8ac5a78c9f1a05dbecb
beakjinu <dighwlsdn96@naver.com>
2025-11-07 01:03:46 +0900

    Image, Card

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,38 +6,50 @@ import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            val scrollState = rememberScrollState()
-
-            Column(
-                modifier = Modifier
-                    .systemBarsPadding()
-                    .verticalScroll(scrollState)
-            ){
-
-                Text("1번 텍스트")
-                Spacer(modifier = Modifier.height(10.dp))
-                Text("2번 텍스트")
-                for (i in 1..50){
-                    Text("글씨 ${i}")
-                }
+            ImageCard()
+        }
+    }
+    @Composable
+    fun ImageCard(){
+        Card(
+            shape = RoundedCornerShape(8.dp),
+            elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)
+        ){
+            Box(
+                modifier = Modifier.height(200.dp)
+                    .fillMaxWidth()
+            )
+            {
+                Image(painter = painterResource(id = R.drawable.ic_launcher_background),//R? = Resource
+                    contentDescription = "poster",
+                    contentScale = ContentScale.Crop
+                    )
             }
         }
     }
