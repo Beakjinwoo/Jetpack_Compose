d670655584962ef21f455a296014cc978a488781
beakjinwoo <bjw@smonster.kr>
2025-11-14 12:04:02 +0900

    #fix : GET 요청 @BODY -> @QUERY

--- app/src/main/java/com/example/myapplication/apiservice/ProductApi.kt
+++ app/src/main/java/com/example/myapplication/apiservice/ProductApi.kt
@@ -1,18 +1,16 @@
 package com.example.myapplication.apiservice
 
-import com.example.myapplication.data.product.ProductRequest
+import com.example.myapplication.data.product.Product
 import com.example.myapplication.data.product.ProductResponse
-import retrofit2.http.Body
 import retrofit2.http.GET
-import retrofit2.http.Header
-
+import retrofit2.http.Query
+import retrofit2.Response
 interface ProductApi {
 
+    //get 요청에서 @BODY가 아니라 @Query 사용해야한다
     @GET("/product")
-    suspend fun GetProducts(@Body request: ProductRequest)
-    : List<ProductResponse>
-
-//    @GET("/product")
-//    suspend fun GetProducts(@Header ("Authorization") accessToken: String , @Body request: ProductRequest)
-//            : List<ProductResponse>
+    suspend fun GetProducts(
+        @Query("after") after: String,
+        @Query("count") count: Int
+    ): Response<ProductResponse<List<Product>>>
 }
