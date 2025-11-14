a178bf7801f01398c63ea9574c12d3bf1b11a34a
beakjinwoo <bjw@smonster.kr>
2025-11-14 12:03:06 +0900

    #fix : 응답 데이터 타입 맞춤

--- app/src/main/java/com/example/myapplication/state/ProductApiState.kt
+++ app/src/main/java/com/example/myapplication/state/ProductApiState.kt
@@ -1,7 +1,6 @@
 package com.example.myapplication.state
 
-import com.example.myapplication.apiservice.ProductApi
-import com.example.myapplication.data.product.ProductResponse
+import com.example.myapplication.data.product.Product
 
 sealed class ProductApiState {
 
@@ -9,7 +8,7 @@ sealed class ProductApiState {
 
     object Loading : ProductApiState()
 
-    data class Success(val response: ProductApiState) : ProductApiState()
+    data class Success(val response: List<Product>) : ProductApiState()
 
     data class Error(val message: String) : ProductApiState()
 
