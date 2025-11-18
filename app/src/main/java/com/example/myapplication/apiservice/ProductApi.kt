acc26c24a02eaa3f2f675a4db45949ec183b4d04
beakjinwoo <bjw@smonster.kr>
2025-11-18 11:46:49 +0900

    #add : 로그인 방식 변경

--- app/src/main/java/com/example/myapplication/apiservice/ProductApi.kt
+++ app/src/main/java/com/example/myapplication/apiservice/ProductApi.kt
@@ -8,20 +8,11 @@ import retrofit2.Response
 import retrofit2.http.Header
 import retrofit2.http.POST
 
-data class RefreshTokenRequest(val refreshToken: String)
-data class TokenResponse(
-    val accessToken: String,
-)
 interface ProductApi {
     //get 요청에서 @BODY가 아니라 @Query 사용해야한다
     @GET("/product")
-    suspend fun GetProducts(
+    suspend fun getProducts(
         @Query("after") after: String,
         @Query("count") count: Int
-    ): Response<ProductResponse<List<Product>>>
-
-    @POST("/auth/token")
-    suspend fun refreshToken(
-        @Header("Authorization") refreshToken: String
-    ): Response<TokenResponse>
+    ): Response<ProductResponse>
 }
